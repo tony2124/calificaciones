23cd953ac39dbf78c658298b4e1e9c0e61691062
Alfonso Calderon <alfonso.calderon.chavez@gmail.com>
2012-06-07 14:56:26 -0500

    Foundation framework

--- www/lib/themes/default/footer.php
+++ www/lib/themes/default/footer.php
@@ -7,14 +7,10 @@
 		include "mobile/footer.php";
 	} else {
 ?>
-			</div>
-		</div>
-
 		<footer>
 			<p>&copy; <?php print __("All rights reserved"); ?> - ZanPHP Framework v.2.5 - 2011 - <?php print __("Powered by"); ?> <a href="http://www.milkzoft.com" title="MilkZoft">MilkZoft</a></p>
 		</footer>
-	  
-		</div>
+
 	</body>
 </html>
 <?php } ?>
