423ae55d6af6261deede51fbf91f21af015133fa
unknown <SIMPUS@SIMPUS-PC.(none)>
2012-06-07 15:14:45 -0500

    login

--- www/applications/default/views/Login.php
+++ www/applications/default/views/Login.php
@@ -1,15 +1,9 @@
 <div class="longinA">
 	<p class="titulo_log">Inciar sesión</p>
 	<hr size="3px" color="gray">
-	<div class="log_Iz">
-		Usuario: 
-		<br><br>
-		Contraseña:
-	</div>
-
-	<div class="log_der">
-		<input type="text" name="user">
-		<br><br>
-		<input type="text" name="password">
-	</div>
+		Usuario: <input type="text" name="user">
+		<br>
+		Contraseña: <input type="text" name="password">
+<br>
+		
 </div>
